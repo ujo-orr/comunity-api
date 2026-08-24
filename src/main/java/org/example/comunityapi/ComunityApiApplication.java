1fa2c4c7e29147434040a304751955b7fc017aef
woogie <woke55@naver.com>
2026-08-24 13:16:49 +0900

    FEAT : 프로젝트 초기화 및 Gradle 의존성 설정 (Spring Boot 3.3.5, Java 21)

---
+++ src/main/java/org/example/comunityapi/ComunityApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.comunityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComunityApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/comunityapi/ComunityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.comunityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
